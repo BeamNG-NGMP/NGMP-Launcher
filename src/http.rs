c1e273c76c9fe124d1a7523fc4ed91463d362396
Luuk van Oijen <lazyluuk.channel@gmail.com>
2024-10-26 00:32:59 +0200

    implement login stuff

--- src/http.rs
+++ src/http.rs
@@ -1,13 +1,37 @@
 use warp::Filter;
 use serde::{Serialize, Deserialize};
+use tokio::sync::mpsc;
 
 use crate::signal::SignalReceiver;
 
-pub fn http_main(kill_signal: SignalReceiver, http_port: u16) {
+// const LOGIN_API: &'static str = "http://127.0.0.1:11281";
+// const LOGIN_API: &'static str = "http://login.ngmp.net:11281";
+const LOGIN_API: &'static str = "http://138.201.33.234:11281";
+
+#[derive(Debug)]
+pub enum Message {
+    AuthFailure,
+    AuthData(UserAuth),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserAuth {
+    pub auth: String,
+    pub steam_id: u64,
+    pub user: User,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub name: String,
+    pub avatar_hash: String,
+}
+
+pub fn http_main(kill_signal: SignalReceiver, http_port: u16, message_tx: mpsc::Sender<Message>) {
     info!("Http server started!");
 
     let rt = tokio::runtime::Runtime::new().expect("Failed to create runtime for HTTP server!");
-    let handle = rt.spawn(async move { http_async(http_port).await; });
+    let handle = rt.spawn(async move { http_async(http_port, message_tx).await; });
 
     loop {
         if !kill_signal.alive() {
@@ -22,8 +46,50 @@ pub fn http_main(kill_signal: SignalReceiver, http_port: u16) {
     info!("Http server stopped!");
 }
 
-async fn http_async(http_port: u16) {
-    let index = warp::path::end().map(|| "hi how'd you get here!!!");
+async fn http_async(http_port: u16, message_tx: mpsc::Sender<Message>) {
+    let index = warp::path::end().and_then(|| async move { Ok::<_, warp::Rejection>("hi how'd you get here!!!") });
+
+    let login = warp::path("login").and_then(|| async move {
+        let redirector = steam_auth::Redirector::new("http://localhost:4434", "/login_callback").unwrap();
+
+        Ok::<_, warp::Rejection>(warp::reply::with_header(
+            warp::reply::with_status("", warp::http::StatusCode::FOUND),
+            "Location",
+            redirector.url().as_str()
+        ))
+    });
+
+    let login_callback = warp::path("login_callback").and(warp::query::raw()).and_then(move |q| {
+        let tx = message_tx.clone();
+        async move {
+            let endpoint = format!("{LOGIN_API}/login?{}", q);
+
+            let client = reqwest::Client::new();
+            match client.get(endpoint)
+                .send()
+                .await
+            {
+                Err(e) => {
+                    error!("{}", e);
+                    tx.send(Message::AuthFailure).await;
+                    Ok::<_, warp::Rejection>("Error occured :(")
+                },
+                Ok(res) => {
+                    match res.json::<UserAuth>().await {
+                        Err(e) => {
+                            error!("{}", e);
+                            tx.send(Message::AuthFailure).await;
+                            Ok::<_, warp::Rejection>("Error occured :(")
+                        },
+                        Ok(user_auth) => {
+                            tx.send(Message::AuthData(user_auth)).await;
+                            Ok::<_, warp::Rejection>("Success! You can now close this page.")
+                        }
+                    }
+                },
+            }
+        }
+    });
 
     let server_list = warp::path("server_list").map(|| {
         let server_list = get_server_list();
@@ -32,6 +98,8 @@ async fn http_async(http_port: u16) {
 
     let routes = warp::get().and(
         index
+            .or(login)
+            .or(login_callback)
     );
 
     warp::serve(routes)
@@ -41,7 +109,7 @@ async fn http_async(http_port: u16) {
 
 #[derive(Serialize, Deserialize)]
 pub struct ServerList {
-    
+
 }
 
 fn get_server_list() -> ServerList {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate log;
 
 use std::net::SocketAddr;
+use tokio::sync::mpsc;
 
 use ngmp_protocol_impl::{
     connection::*,
@@ -26,11 +27,17 @@ async fn main() {
 
     let config = config::load_config();
 
+    // Load the stored auth code (if possible)
+    let access_token = std::fs::read_to_string("key").ok();
+
+    // Set up a messaging channel for HTTP->Launcher messages
+    let (http_msg_tx, mut http_msg_rx) = mpsc::channel(25);
+
     // When the main server thread stops running, this signal is automatically dropped.
     let (kill_signal, kill_receiver) = Signal::new();
     {
         let signal_rx = kill_receiver.clone();
-        std::thread::spawn(move || http::http_main(signal_rx, config.networking.http_port));
+        std::thread::spawn(move || http::http_main(signal_rx, config.networking.http_port, http_msg_tx));
     }
 
     // Start the UDP listener for the client - launcher connection
@@ -40,7 +47,14 @@ async fn main() {
         info!("Waiting for game to connect on port {}!", config.networking.launcher_port);
         match launcher_listener.wait_for_packet().await {
             Ok((packet, local_addr)) => match packet {
-                ClientPacket::ClientInfo(client_info) => launcher_main(&mut launcher_listener, local_addr, client_info, kill_receiver.clone()).await,
+                ClientPacket::ClientInfo(client_info) => launcher_main(
+                    &mut launcher_listener,
+                    local_addr,
+                    client_info,
+                    kill_receiver.clone(),
+                    access_token.clone(),
+                    &mut http_msg_rx
+                ).await,
                 _ => error!("Unknown packet sent! Packet: {:?}", packet),
             },
             Err(e) => error!("Failed to receive packet from client: {}", e),
@@ -52,7 +66,9 @@ async fn launcher_main(
     launcher_socket: &mut UdpListener<ClientPacket>,
     local_addr: SocketAddr,
     client_info: launcher_client::handshake::ClientInfoPacket,
-    signal_rx: SignalReceiver
+    signal_rx: SignalReceiver,
+    access_token: Option<String>,
+    http_msg_rx: &mut mpsc::Receiver<http::Message>,
 ) {
     info!("Game connected!");
 
@@ -61,15 +77,81 @@ async fn launcher_main(
         protocol_version: 1,
     })).await;
 
+    // Load potential auth data
+    let user_info = if let Some(auth_token) = access_token {
+        // TODO: Implement requesting steam id and name
+        todo!()
+    } else {
+        trace!("Player is not currently logged in!");
+
+        // AuthenticationInfo packet
+        launcher_socket.write_packet(local_addr, ClientPacket::AuthenticationInfo(launcher_client::handshake::AuthenticationInfoPacket {
+            confirm_id: 2,
+            success: false,
+            player_name: String::new(),
+            steam_id: String::new(),
+            avatar_hash: String::new(),
+        })).await;
+
+        // Wait for LoginRequest packet
+        match launcher_socket.wait_for_packet().await {
+            Ok((packet, _)) => match packet {
+                ClientPacket::LoginRequest => {}, // Yipee,
+                _ => {
+                    error!("Unknown packet received, expected Packet::LoginRequest, got {:?}", packet);
+                    return;
+                }
+            },
+            Err(e) => {
+                error!("Failed to receive packet from client: {}", e);
+                return;
+            },
+        }
+
+        if let Err(e) = open::that("http://localhost:4434/login") {
+            error!("Failed to open link: {}", e);
+            info!("Please open the link manually: http://localhost:4434/login");
+        }
+
+        // Now we wait for the login data from the http thread
+        match http_msg_rx.recv().await {
+            None => {
+                error!("The messaging channel from the http server has closed! Unrecoverable error :(");
+                return;
+            },
+            Some(msg) => {
+                match msg {
+                    http::Message::AuthData(v) => v,
+                    http::Message::AuthFailure => {
+                        // TODO: We should simply retry the authentication if this fails
+                        error!("Failed to authenticate, currently unrecoverable.");
+                        return;
+                    },
+                    _ => {
+                        error!("Unsupported message sent, uh oh!");
+                        return;
+                    }
+                }
+            },
+        }
+    };
+    trace!("Logged in: {:#?}", user_info);
+
+    // Now we can cache the auth token to disk
+    // TODO: Do that
+
+    // AuthenticationInfo packet
     launcher_socket.write_packet(local_addr, ClientPacket::AuthenticationInfo(launcher_client::handshake::AuthenticationInfoPacket {
         confirm_id: 2,
         success: true,
-        player_name: "mista gaming".to_string(),
+        player_name: user_info.user.name,
+        steam_id: user_info.steam_id.to_string(),
+        avatar_hash: user_info.user.avatar_hash,
     })).await;
 
     loop {
         match launcher_socket.wait_for_packet().await {
-            Ok((packet, _addr)) => match packet {
+            Ok((packet, _)) => match packet {
                 ClientPacket::ReloadLauncherConnection => {
                     info!("Client mod disconnected! Waiting for it to reconnect...");
                     break;
